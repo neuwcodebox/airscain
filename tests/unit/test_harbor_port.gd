1ab1b13b0269eedfff1156080d4ce61d4e44ddb8
Neuw15611cbox <neuwgmbox@gmail.com>
2026-09-26 00:50:02 +0900

    test: reject nonfinite harbor route samples

--- tests/unit/test_harbor_port.gd
+++ tests/unit/test_harbor_port.gd
@@ -21,12 +21,12 @@ func test_each_battlefield_has_a_water_route_between_the_fog_and_a_coastal_berth
 		assert_gt(-route.inbound_pose(route.inbound_duration).basis.z.dot(inbound_direction), 0.99, "%s: 선수는 접안 방향을 향합니다" % layout_id)
 		for index: int in range(0, route.inbound.size(), 8):
 			var point := route.inbound[index]
-			if field.terrain_height(point.x, point.z) >= route.sea_level - HarborRoute.MINIMUM_WATER_DEPTH:
+			if not (field.terrain_height(point.x, point.z) < route.sea_level - HarborRoute.MINIMUM_WATER_DEPTH):
 				fail_test("%s: inbound %d crosses shallow water" % [layout_id, index])
 				return
 		for index: int in range(0, route.outbound.size(), 8):
 			var point := route.outbound[index]
-			if field.terrain_height(point.x, point.z) >= route.sea_level - HarborRoute.MINIMUM_WATER_DEPTH:
+			if not (field.terrain_height(point.x, point.z) < route.sea_level - HarborRoute.MINIMUM_WATER_DEPTH):
 				fail_test("%s: outbound %d crosses shallow water" % [layout_id, index])
 				return
 
@@ -135,10 +135,10 @@ func test_voyages_use_separate_water_approaches_and_turn_continuously() -> void:
 			for index: int in range(1, times.size() - 1):
 				var before := voyage.inbound_pose(times[index] - 0.001) if entering else voyage.outbound_pose(times[index] - 0.001)
 				var after := voyage.inbound_pose(times[index] + 0.001) if entering else voyage.outbound_pose(times[index] + 0.001)
-				if before.basis.get_rotation_quaternion().angle_to(after.basis.get_rotation_quaternion()) >= 0.002:
+				if not (before.basis.get_rotation_quaternion().angle_to(after.basis.get_rotation_quaternion()) < 0.002):
 					fail_test("항로 %d 진입 %s 표본 %d에서 방향이 불연속입니다" % [variants.find(voyage), entering, index])
 					return
-				if field.terrain_height(after.origin.x, after.origin.z) >= voyage.sea_level - HarborRoute.MINIMUM_WATER_DEPTH:
+				if not (field.terrain_height(after.origin.x, after.origin.z) < voyage.sea_level - HarborRoute.MINIMUM_WATER_DEPTH):
 					fail_test("항로 %d 진입 %s 표본 %d에서 수심이 부족합니다" % [variants.find(voyage), entering, index])
 					return
 		var dock := voyage.inbound_pose(voyage.inbound_duration)
